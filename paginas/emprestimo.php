b8a02c08f3daddd7dea333bcf38fc358f02cbbce
Antonio Eduardo <56364149+AntonioEduardo-sm@users.noreply.github.com>
2020-09-17 11:04:37 -0300

    emprestimo

--- paginas/emprestimo.php
+++ paginas/emprestimo.php
@@ -46,7 +46,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: rgba(61, 201, 179, 1);">
     <div class="container">
-      <a class="navbar-brand" href="index.php">Biblioteca</a>
+      <a class="navbar-brand" href="../index.php">Biblioteca</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
